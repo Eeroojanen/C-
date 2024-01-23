d3ca567dbed0719023032a3f89402f87c917a45c
Eero Ojanen <124070597+Eeroojanen@users.noreply.github.com>
2024-01-23 14:23:10 +0200

    Add files via upload

---
+++ Assignment_3/Assignment_3/Concert.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment_3
+{
+    internal class Concert
+    {
+        string title;
+        string location;
+        int date;
+        string time;
+        double price;
+
+
+        //Constructor for concert class
+        public Concert(string title, string location, int date, string time, double price)
+        {
+            this.title = title;
+            this.location = location;
+            this.date = date;
+            this.time = time;
+            this.price = price;
+
+        }
+
+        //Return value of title field
+        public string Title()
+        {
+            return this.title;
+        }
+
+        //Return value of price field
+        public double Price()
+        {
+            return this.price;
+        }
+
+        public static void Sort(ArrayList concertArray)
+        {
+            // Loop through the elements in the concertArray
+            for (int i = 0; i < concertArray.Count - 1; i++)
+            {
+                // Loop through the remaining unsorted elements
+                for (int j = 0; j < concertArray.Count - i - 1; j++)
+                {
+                    // Compare two concerts by their price
+                    Concert concertA = (Concert)concertArray[j];
+                    Concert concertB = (Concert)concertArray[j + 1];
+
+                    if (concertA.Price() > concertB.Price())
+                    {
+                        // Swap the concerts if they are out of order
+                        concertArray[j] = concertB;
+                        concertArray[j + 1] = concertA;
+                    }
+                }
+            }
+        }
+
+        //Operator where we chech if prices are equal
+        public static bool operator ==(Concert concert1, Concert concert2)
+        {
+            if (concert1.price.Equals(concert2.price) && concert1.price == concert2.price)
+                return true;
+            return false;
+        }
+
+        // Operator that checks if prices of two Concert objects are not equal
+        public static bool operator !=(Concert concert1, Concert concert2)
+        {
+            if (!(concert1.price.Equals(concert2.price) && concert1.price == concert2.price))
+                return true;
+            return false;
+        }
+
+
+        //Operator where if check if the price is cheaper
+        public static bool operator <(Concert concert5, Concert concert4)
+        {
+            if (concert5.price < concert4.price)
+                return true;
+            return false;
+        }
+
+        //Operator where if check if the price is higher
+        public static bool operator >(Concert concert3, Concert concert4)
+        {
+            if (concert3.price > concert4.price)
+                return true;
+            return false;
+        }
+
+        //Operator where we add +5 unit to the price
+        public static Concert operator ++(Concert concert)
+          {
+              concert.price += 5;
+            return concert;
+          }
+
+        //Operator where we decrease -5 unit to the price
+        public static Concert operator --(Concert concert)
+          {
+              concert.price -= 5;
+            if (concert.price < 0)
+                concert.price = 0;
+
+            return concert;
+          }
+
+        public override string ToString()
+        {
+            return Title() + " " + location + " " + date + " " + time + " " + Price();
+        }
+
+    }
+}
---
+++ Assignment_3/Assignment_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment_3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList ConcertArray = new ArrayList();
+
+            Concert concert1 = new Concert("Vasafest", "Vaasa", 2023,"17:00", 200);
+            Concert concert2 = new Concert("Provinssirock", "Seinäjoki", 2022, "15.00", 200);
+            Concert concert3 = new Concert("Weekend Festival", "Helsinki", 2018, "14.00", 900);
+            Concert concert4 = new Concert("RuisRock", "Turku", 2016, "20:30", 400);
+            Concert concert5 = new Concert("TampereFest", "Tampere", 2015, "13:00", 800);
+
+            ConcertArray.Add(concert1);
+            ConcertArray.Add(concert2);
+            ConcertArray.Add(concert3);
+            ConcertArray.Add(concert4);
+            ConcertArray.Add(concert5);
+
+            //Sorting array
+            Concert.Sort(ConcertArray);
+            //Printing the concent of array
+            foreach (Concert concert in ConcertArray) 
+            { 
+                Console.WriteLine(concert);
+            }
+
+            Console.WriteLine("Are the concert1 and concert2 prices equal? " + concert1.Price().Equals(concert2.Price()));
+            Console.WriteLine("Is concer 5 cheaper? " + (concert5 < concert4));
+            Console.WriteLine("Is the concert 3 more expensive ?" + (concert3 > concert4));
+            Console.WriteLine("Increase the concert4 price of by 5 units " + (++concert4));
+            Console.WriteLine("Decrease the concert3 price of by 5 units " + (--concert3));
+
+            
+        }
+
+    }
+}
